3e25a7ba9197bb28d8fa1c10d1bd847eb802d3df
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-14 12:43:28 -0800

    added cracking the code

--- functiondrills2.js
+++ functiondrills2.js
@@ -6,3 +6,41 @@ function jediName(firstName, lastName){
 }
 
 jediName('Drew', 'Bituin');
+
+function beyond (num){
+    if (num === Number.NEGATIVE_INFINITY || num === Number.POSITIVE_INFINITY){
+        console.log ('And beyond');
+    }
+    if (Number.isFinite(num) && num > 0){
+        console.log('To infinity');
+    }
+    if (Number.isFinite(num) && num < 0){
+        console.log('To negative infinity');
+    }
+    if (num === 0){
+        console.log('Staying home');
+    }
+
+}
+
+function decode (word){
+    switch (word[0]){
+        case 'a':
+            return word[1];
+        case 'b':
+            return word[2];
+        case 'c':
+            return word[3];
+        case 'd':
+            return word[4];
+        default: 
+            return ' ';
+    }
+}
+
+function decodedWords (str){
+    const strArray = str.split(' ');
+    const mappedArray = strArray.map(word => decode(word));
+    const joinedArray = mappedArray.join('');
+    return joinedArray;
+}

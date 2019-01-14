d775fba648aaae2071136f321dbbadcbd8d1d950
JuiceboxJones <jderhalli@gmail.com>
2019-01-14 11:45:58 -0800

    added name/age error checks

--- functiondrills1.js
+++ functiondrills1.js
@@ -6,8 +6,14 @@ function getYearOfBirth(age){
   return `I was born in ${yearOfBirth}` 
 }
 function createGreeting (name, age){
+  if(typeof age !== number){
+    throw new TypeError("Age is not a number");
+  };
+  if(typeof name !== string){
+    throw new TypeError("Name is invalid")
+  };
   return `Hi my name is ${name} and I'm ${age} years old.}`;
-}
+};
 try{
   const greeting1 = createGreeting('Bob', 23);
 } catch(error){

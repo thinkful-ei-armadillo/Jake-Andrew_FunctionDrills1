dfdbb66e9e9a82d112f14c02e2118c2dd78dbd9c
JuiceboxJones <jderhalli@gmail.com>
2019-01-14 11:38:07 -0800

    added try-catch conditional

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,8 +1,16 @@
 function getYearOfBirth(age){
+  if(age<0){
+    throw new Error("Age cannot be negative")
+  }
   const yearOfBirth = 2019 - age;
   return `I was born in ${yearOfBirth}` 
 }
 function createGreeting (name, age){
   return `Hi my name is ${name} and I'm ${age} years old.}`;
 }
+try{
+  const greeting1 = createGreeting('Bob', 23);
+} catch(error){
+console.log('Arguments not valid')
+};
 

07f80a1d0ee41d4025e6f5028bbdb6afba2de653
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-14 13:03:25 -0800

    added month exercise

--- functiondrills2.js
+++ functiondrills2.js
@@ -44,3 +44,48 @@ function decodedWords (str){
     const joinedArray = mappedArray.join('');
     return joinedArray;
 }
+
+function howManyDays (month){
+    let result = '';
+    switch(month){
+        case 'January':
+            result = 'January has 31 days.';
+            break;
+        case 'February':
+            result = 'February has 28 days.';
+            break;
+        case 'March':
+            result = 'March has 31 days.';
+            break;
+        case 'April':
+            result = 'April has 30 days.';
+            break;
+        case 'May':
+            result = 'May has 31 days.';
+            break;
+        case 'June':
+            result = 'June has 30 days.';
+            break;
+        case 'July':
+            result = 'July has 31 days.';
+            break;
+        case 'August':
+            result = 'August has 31 days.';
+            break;
+        case 'September':
+            result = 'September has 30 days.';
+            break;
+        case 'October':
+            result = 'October has 31 days.';
+            break;
+        case 'November':
+            result = 'November has 30 days.';
+            break;
+        case 'December':
+            result = 'December has 31 days.';
+            break;
+        default :
+            result = 'Must provide a valid month.';
+    }
+    return result;
+}
